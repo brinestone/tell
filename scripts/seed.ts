001d9f0ed8e3f0ed6d602be56aa12e0f4b3bcd2a
Conrad Bekondo <conradbekondo@gmail.com>
2025-01-22 16:19:42 +0100

    chore: attempt 4

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -6,10 +6,12 @@ import * as users from '../db/seed/users';
 import * as wallets from '../db/seed/wallets';
 import { DefaultWriter } from '../db/log-writer';
 import { DefaultLogger } from 'drizzle-orm/logger';
-import { Pool } from '@neondatabase/serverless';
+import { neonConfig, Pool } from '@neondatabase/serverless';
+import ws from 'ws';
 
 type Seeder = { name: string, seed: (t: PgTransaction<any>) => Promise<void> }
 const seeders: Seeder[] = [users, categories, wallets];
+neonConfig.webSocketConstructor = global.WebSocket ?? ws;
 
 const logger = process.env['NODE_ENV'] === 'development' ? new DefaultLogger({ writer: new DefaultWriter() }) : false
 console.log('db url = ', process.env['DATABASE_URL'])
