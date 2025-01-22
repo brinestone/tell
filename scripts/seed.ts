4b00f44b8df4dfb629bede11781617a127f610f7
Conrad Bekondo <conradbekondo@gmail.com>
2025-01-22 16:06:45 +0100

    chore: attempt 3

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,23 +1,26 @@
 import 'dotenv/config';
-import { drizzle }       from 'drizzle-orm/neon-serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 import { PgTransaction } from 'drizzle-orm/pg-core';
-import * as categories   from '../db/seed/categories';
-import * as users        from '../db/seed/users';
-import * as wallets      from '../db/seed/wallets';
+import * as categories from '../db/seed/categories';
+import * as users from '../db/seed/users';
+import * as wallets from '../db/seed/wallets';
 import { DefaultWriter } from '../db/log-writer';
 import { DefaultLogger } from 'drizzle-orm/logger';
+import { Pool } from '@neondatabase/serverless';
 
 type Seeder = { name: string, seed: (t: PgTransaction<any>) => Promise<void> }
 const seeders: Seeder[] = [users, categories, wallets];
 
 const logger = process.env['NODE_ENV'] === 'development' ? new DefaultLogger({ writer: new DefaultWriter() }) : false
 console.log('db url = ', process.env['DATABASE_URL'])
-const db = drizzle({ connection: String(process.env['DATABASE_URL']), logger })
-
-db.transaction(async t => {
-  for await (const { seed, name } of seeders) {
-    console.log(`Seeding "${name} ⚙️`);
-    await seed(t as unknown as PgTransaction<any>);
-    console.log(`Seeded "${name}" ✅`);
-  }
-}).catch(console.error);
+const db = drizzle(new Pool({ connectionString: String(process.env['DATABASE_URL']) }), { logger });
+db.$client.connect().then(() =>
+  db.transaction(async t => {
+    for await (const { seed, name } of seeders) {
+      console.log(`Seeding "${name} ⚙️`);
+      await seed(t as unknown as PgTransaction<any>);
+      console.log(`Seeded "${name}" ✅`);
+    }
+  }))
+  .then(() => db.$client.end())
+  .catch(console.error);
